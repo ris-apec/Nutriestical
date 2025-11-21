dd0b5d4cffbb3e9864d11e61018831e872e6f3c3
Tine <ap.christine.nadela@apecschools.edu.ph>
2025-11-21 17:04:29 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -15,9 +15,40 @@ const observer = new IntersectionObserver((entries) => {
     });
 
     gsap.to('.div', {
-        y: 50, 
+        y: -50, 
         scrollTrigger: {
             trigger: '.Scroll1',
             scrub: true,
     }
     })
+
+    /* 2 */
+
+    let slideIndex = 1;
+    showSlides(slideIndex);
+    
+    // Next/previous controls
+    function plusSlides(n) {
+      showSlides(slideIndex += n);
+    }
+    
+    // Thumbnail image controls
+    function currentSlide(n) {
+      showSlides(slideIndex = n);
+    }
+    
+    function showSlides(n) {
+      let i;
+      let slides = document.getElementsByClassName("mySlides");
+      let dots = document.getElementsByClassName("dot");
+      if (n > slides.length) {slideIndex = 1}
+      if (n < 1) {slideIndex = slides.length}
+      for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+      }
+      for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+      }
+      slides[slideIndex-1].style.display = "block";
+      dots[slideIndex-1].className += " active";
+    }
